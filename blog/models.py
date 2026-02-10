294e9894c7738a027d661a2134a624517c4ae216
Muhammadbobur <bobur0218programmer@gmail.com>
2026-02-10 15:56:58 +0500

    feat: project initialized

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now=True)
+  published = models.BooleanField(default=False)
+
+  class Meta:
+    ordering = ['-created_at']
+  
+  def __str__(self):
+    return self.title
