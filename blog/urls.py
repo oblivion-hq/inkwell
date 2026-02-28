85c69bce61b1d94d309f2233dbb1977bf82ea4e8
Muhammadbobur <bobur0218programmer@gmail.com>
2026-02-28 16:16:36 +0500

    feat: post creation form

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home
+from .views import home, post_create
 
 app_name = "blog"
 
 urlpatterns = [
     path("", home, name="blog-home"),
+    path("new/", post_create, name="post-create"),
 ]
