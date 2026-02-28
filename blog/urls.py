1393d31034ee282787970bd7be37311847722a30
Muhammadbobur <bobur0218programmer@gmail.com>
2026-02-28 17:07:52 +0500

    feat: edit and delete actions for post

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import home, post_create, post_detail
+from .views import home, post_create, post_delete, post_detail, post_edit
 
 app_name = "blog"
 
 urlpatterns = [
     path("", home, name="blog-home"),
     path("new/", post_create, name="post-create"),
+    path("<slug:slug>/edit/", post_edit, name="post-edit"),
+    path("<slug:slug>/delete/", post_delete, name="post-delete"),
     path("<slug:slug>/", post_detail, name="post-detail"),
 ]
